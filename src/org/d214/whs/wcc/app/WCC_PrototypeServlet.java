49dbadeb57b108c1c6af3e13bf954b1525f552c1
tciepiela4069 <tciepiela4069@ww-109-17.d214.org>
2014-02-18 08:32:49 -0600

    TopNews

--- src/org/d214/whs/wcc/app/WCC_PrototypeServlet.java
+++ src/org/d214/whs/wcc/app/WCC_PrototypeServlet.java
@@ -2,12 +2,14 @@ package org.d214.whs.wcc.app;
 
 import java.io.IOException;
 import java.util.Iterator;
+import java.util.List;
 
 import javax.servlet.http.*;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
 import org.jsoup.select.Elements;
 
 @SuppressWarnings("serial")
@@ -55,7 +57,7 @@ public class WCC_PrototypeServlet extends HttpServlet {
 		return jsonResponse.toString();
 	}
     
-    private static String getDailyAnnouncements()
+    private String getDailyAnnouncements()
     {
         StringBuffer jsonResponse = new StringBuffer("[");
         
@@ -65,16 +67,49 @@ public class WCC_PrototypeServlet extends HttpServlet {
             Element table = doc.select(".bdywrpr .corwrpr-2clm-lr .cormain-2clm-lr table").first(); //still do
             
             Iterator<Element> ite = table.select("td").iterator();
-            String string = ite.next().text();
-            System.out.println(string);
-            
-            /*for (Iterator iterator = eventItems.iterator(); iterator.hasNext();)
+            while (ite.hasNext())
             {
-                Element element = (Element) iterator.next();
-                String elementText = element.text();
-                elementText = elementText.replace('"', '\'');
-                jsonResponse.append(String.format("\"%s\",", elementText));
-            }*/
+            	ite.next();
+            	String s = ite.next().text();
+            	jsonResponse.append(s);
+            	System.out.println(s);
+            }
+            
+            jsonResponse.deleteCharAt(jsonResponse.length() - 1);
+        }
+        catch (Exception exc)
+        {
+            jsonResponse.append("There was an error retrieving the daily announcements.");
+        }
+        
+        jsonResponse.append("]");
+        return jsonResponse.toString();
+    }
+    
+    private String getTopNews()
+    {
+        StringBuffer jsonResponse = new StringBuffer("[");
+        
+        try
+        {
+            Document doc = Jsoup.connect("http://whs.d214.org/").get();
+            //Elements table = doc.select("div#CT_Main_0_cache_pnlModule");
+            Element table = doc.select(".bdywrpr .corwrpr-3clm .wrpr2clm .cormain-hm #CT_Main_0_cache_pnlModule").first();
+            List<Node> childNodes = table.childNodes();
+            //Iterator<Element> ite = table.select("td").iterator();
+            Iterator<Node> ite = childNodes.iterator();
+            
+            while (ite.hasNext())
+            {
+            	//ite.next();
+            	Node n = ite.next();
+            	String s = n.nodeName();
+            	String[] one;
+            	String[] two;
+            	jsonResponse.append(s);
+            	System.out.println(s);
+            	System.out.println("****");
+            }
             
             jsonResponse.deleteCharAt(jsonResponse.length() - 1);
         }
