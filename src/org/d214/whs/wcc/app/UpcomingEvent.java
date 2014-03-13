cc5333406ab798fe8c15f8d993f38a04132805a2
tciepiela4069 <tciepiela4069@w-109-17.d214.org>
2014-03-13 09:41:43 -0500

    Updated code; TopNews still needs work

--- src/org/d214/whs/wcc/app/UpcomingEvent.java
+++ src/org/d214/whs/wcc/app/UpcomingEvent.java
@@ -1,13 +1,14 @@
+package org.d214.whs.wcc.app;
+
 public class UpcomingEvent
 {
-    public String eventDate, eventTitle, eventDetails;
+	public String eventDate, eventTitle;
     
-    public UpcomingEvent(String date, String title, String details)
+    public UpcomingEvent(String date, String title)
     {
-        if ((date == null) || (title == null) || (details == null))
+        if ((date == null) || (title == null))
             throw new NullPointerException();
         eventDate = date;
         eventTitle = title;
-        eventDetails = details;
     }
 }
