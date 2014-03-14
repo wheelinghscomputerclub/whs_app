1e00531c846f8c72f7f36bcda282caa26410a59d
tciepiela4069 <tciepiela4069@ww-109-17.d214.org>
2014-03-14 07:31:33 -0500

    TopNews class; WheelingApp update on what I have so far on parsing the TopNews

---
+++ src/org/d214/whs/wcc/app/TopNews.java
@@ -0,0 +1,14 @@
+package org.d214.whs.wcc.app;
+
+public class TopNews
+{
+    private String title, details;
+    
+    public TopNews(String newsTitle, String newsDetails)
+    {
+        if ((newsTitle == null) || (newsDetails == null))
+            throw new NullPointerException();
+        title = newsTitle;
+        details = newsDetails;
+    }
+}
